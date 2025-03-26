2765833dafd496f2043cee8b573a478c347ca272
Brayan272096 <urregob100@gmail.com>
2025-03-26 14:21:59 -0500

    Add files via upload

---
+++ paginasss/login.js
@@ -0,0 +1,15 @@
+function pruebaContinuar(){
+    alert("¡Bienvenido!");
+    confirmacion = document.getElementById("confirmacion").value;
+    window.location.href = "principalmenu.html";
+    
+
+
+}
+
+function continuar(){
+alert("¡Bienvenido!");
+confirmacionn = document.getElementById("confirmacionn");
+window.location.href = "principalmenu.html";
+
+}
